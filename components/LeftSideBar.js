7d30f7ac235ac48ddf195270d0e6f5c85a2e60d3
thedoru <thedoruv2@gmail.com>
2023-02-25 18:12:58 +0100

    finished sidebar design

--- components/LeftSideBar.js
+++ components/LeftSideBar.js
@@ -2,7 +2,10 @@ import Link from "next/link";
 
 export default function LeftSidebar() {
   return (
-    <div className="flex flex-col p-4 w-3/12">
+    <div
+      style={{ height: "calc(100vh - 64px)", top: "62px" }}
+      className="py-4 pl-2 h-full w-[320px] m-0 sticky"
+    >
       <Link href="/profile/test">
         <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
           <div className="w-9 h-9 bg-red-400 rounded-full"></div>
--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -1,7 +1,20 @@
-export default function RightSidebar() {
+export default function RightSidebar({ friends }) {
   return (
-    <div>
-      <p className="font-bold text-lg">Contacts</p>
+    <div
+      style={{ height: "calc(100vh - 64px)", top: "62px" }}
+      className="w-[320px] sticky flex flex-col pr-2"
+    >
+      <div className="p-2">
+        <p className="font-bold text-lg">Contacts</p>
+      </div>
+      {friends.map((item) => {
+        return (
+          <div className="px-2 py-2 rounded-lg flex items-center gap-3 hover:bg-stone-800">
+            <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+            <p className="text-gray-300">{item.user}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
