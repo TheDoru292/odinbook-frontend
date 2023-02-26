aea22bc47c20aaf3c4bb05469b5fb0eaa3603d58
thedoru <thedoruv2@gmail.com>
2023-02-26 11:27:38 +0100

    Profile friends responsive layout

--- pages/profilefriends.js
+++ pages/profilefriends.js
@@ -12,7 +12,7 @@ export default function ProfileFriends() {
       <Header />
       <main className="text-stone-200 flex flex-col w-100 flex-grow bg-stone-900">
         <ProfileData currentPage={"friends"} />
-        <div className="flex px-64 gap-4 py-4 w-100">
+        <div className="flex px-2 xl:px-64 gap-4 py-4 w-100">
           <div className="w-full flex flex-col gap-6 bg-stone-800 rounded-xl px-4 py-3">
             <div className="flex">
               <h2 className="flex-grow text-lg font-bold">Friends</h2>
@@ -21,39 +21,39 @@ export default function ProfileFriends() {
                 <p>Find friends</p>
               </div>
             </div>
-            <div className="flex w-100 flex-wrap px-6">
-              <div className="flex gap-4 w-6/12 mb-6 pr-6">
-                <div className="w-20 h-20 bg-red-400 rounded-lg"></div>
+            <div className="flex flex-wrap w-full flex-wrap md:px-6">
+              <div className="flex gap-4 w-full md:w-6/12 mb-6 md:pr-6">
+                <div className="w-14 h-14 md:w-20 md:h-20 bg-red-400 rounded-lg"></div>
                 <p className="self-center flex-grow font-bold">Hii</p>
-                <p className="self-center border-sky-400 text-sky-400 border p-2 rounded-lg">
+                <p className="self-center border-sky-400 text-sky-400 border p-1 md:p-2 rounded-lg">
                   <span>✓</span>Friends
                 </p>
               </div>
-              <div className="flex gap-4 w-6/12 mb-6 pr-6">
-                <div className="w-20 h-20 bg-red-400 rounded-lg"></div>
+              <div className="flex gap-4 w-full md:w-6/12 mb-6 md:pr-6">
+                <div className="w-14 h-14 md:w-20 md:h-20 bg-red-400 rounded-lg"></div>
                 <p className="self-center flex-grow font-bold">Hii</p>
-                <p className="self-center border-sky-400 text-sky-400 border p-2 rounded-lg">
+                <p className="self-center border-sky-400 text-sky-400 border p-1 rounded-lg">
                   <span>✓</span>Friends
                 </p>
               </div>
-              <div className="flex gap-4 w-6/12 mb-6 pr-6">
-                <div className="w-20 h-20 bg-red-400 rounded-lg"></div>
+              <div className="flex gap-4 w-full md:w-6/12 mb-6 md:pr-6">
+                <div className="w-14 h-14 md:w-20 md:h-20 bg-red-400 rounded-lg"></div>
                 <p className="self-center flex-grow font-bold">Hii</p>
-                <p className="self-center border-sky-400 text-sky-400 border p-2 rounded-lg">
+                <p className="self-center border-sky-400 text-sky-400 border p-1 rounded-lg">
                   <span>✓</span>Friends
                 </p>
               </div>
-              <div className="flex gap-4 w-6/12 mb-6 pr-6">
-                <div className="w-20 h-20 bg-red-400 rounded-lg"></div>
+              <div className="flex gap-4 w-full md:w-6/12 mb-6 md:pr-6">
+                <div className="w-14 h-14 md:w-20 md:h-20 bg-red-400 rounded-lg"></div>
                 <p className="self-center flex-grow font-bold">Hii</p>
-                <p className="self-center border-sky-400 text-sky-400 border p-2 rounded-lg">
+                <p className="self-center border-sky-400 text-sky-400 border p-1 rounded-lg">
                   <span>✓</span>Friends
                 </p>
               </div>
-              <div className="flex gap-4 w-6/12 mb-6 pr-6">
-                <div className="w-20 h-20 bg-red-400 rounded-lg"></div>
+              <div className="flex gap-4 w-full md:w-6/12 mb-6 md:pr-6">
+                <div className="w-14 h-14 md:w-20 md:h-20 bg-red-400 rounded-lg"></div>
                 <p className="self-center flex-grow font-bold">Hii</p>
-                <p className="self-center border-sky-400 text-sky-400 border p-2 rounded-lg">
+                <p className="self-center border-sky-400 text-sky-400 border p-1 rounded-lg">
                   <span>✓</span>Friends
                 </p>
               </div>
