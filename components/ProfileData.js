e3e668b3851ceb5fa8a2c79f7e61c2cf7056f915
thedoru <thedoruv2@gmail.com>
2023-02-26 11:19:52 +0100

    profile page responsive layout

--- components/ProfileData.js
+++ components/ProfileData.js
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function ProfileData({ currentPage }) {
   const postsStyling =
     currentPage == "posts"
-      ? "border-b border-sky-400 rounded-t-md hover:bg-stone-600 px-2 py-3"
+      ? "border-b border-sky-400 rounded-t-md text-sky-400 hover:bg-stone-600 px-2 py-3"
       : "hover:bg-stone-600 rounded-md px-2 py-3";
 
   const friendsStyling =
     currentPage == "friends"
-      ? "border-b border-sky-400 rounded-t-md hover:bg-stone-600 px-2 py-3"
+      ? "border-b border-sky-400 rounded-t-md text-sky-400 hover:bg-stone-600 px-2 py-3"
       : "hover:bg-stone-600 rounded-md px-2 py-3";
 
   return (
-    <div className="flex flex-col gap-4 px-64 pt-4 bg-stone-800">
+    <div className="flex flex-col gap-4 px-2 xl:px-64 pt-4 bg-stone-800">
       <div className="flex gap-4">
         <div className="w-16 h-16 bg-red-400 rounded-full"></div>
         <div className="flex flex-col">
--- pages/profile.js
+++ pages/profile.js
@@ -9,16 +9,16 @@ export default function Profile() {
       <Header />
       <main className="relative text-stone-200 flex flex-col flex-grow h-100 bg-stone-900">
         <ProfileData currentPage={"posts"} />
-        <div className="px-64 gap-4 flex">
-          <aside
-            style={{ height: "calc(100vh - 64px)", top: "66px" }}
-            className="hidden lg:flex sticky m-0 w-5/12 flex-col gap-4 py-4"
-          >
+        <div className="px-2 xl:px-64 gap-4 flex">
+          <aside className="hidden md:flex m-0 w-5/12 flex-col gap-4 py-4">
             <div className="bg-stone-800 rounded-xl px-4 py-3">
               <h2 className="text-lg font-bold">Intro</h2>
               <p>No information</p>
             </div>
-            <div className="bg-stone-800 rounded-xl px-4 py-3">
+            <div
+              style={{ top: "66px" }}
+              className="sticky bg-stone-800 rounded-xl px-4 py-3"
+            >
               <div className="flex">
                 <h2 className="flex-grow text-lg font-bold">Friends</h2>
                 <p className="text-lg ">See All Friends</p>
@@ -26,39 +26,39 @@ export default function Profile() {
               <p>69 friends</p>
               <div className="mt-2 flex flex-wrap gap-4">
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>User</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>PowerPlay</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>Ey</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>Hey</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>Test</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>Aaaa</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>O</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>T</p>
                 </div>
                 <div className="flex-basis flex-grow">
-                  <div className="w-28 h-28 bg-red-400 rounded-md"></div>
+                  <div className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"></div>
                   <p>Test 2</p>
                 </div>
               </div>
