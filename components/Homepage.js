bfaa8fb1dfbd2e5dd171ecbaca9ce7ef67f4cfaf
thedoru <thedoruv2@gmail.com>
2023-02-27 23:51:11 +0100

    fetch posts from backend

--- components/Homepage.js
+++ components/Homepage.js
@@ -5,40 +5,33 @@ import RightSidebar from "./RightSidebar";
 import MainPagePost from "./PostDiv";
 import Post from "./Post";
 import Menu from "./Menu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreatePost from "./CreatePost";
 
-const data = {
-  posted_on: new Date(),
-  content: "Heyyy",
-};
-
-const likes = {
-  count: 23,
-};
-
-const comments = [
-  {
-    user: "test 1",
-    content: "Nice!",
-    commented_on: new Date(),
-  },
-];
-
-const friends = [
-  {
-    user: "Ey",
-  },
-  {
-    user: "PowerPlay",
-  },
-  { user: "Hey" },
-  { user: "Test" },
-];
-
-export default function Homepage() {
+export default function Homepage({ user }) {
   const [openMenu, setOpenMenu] = useState(false);
   const [openPost, setOpenPost] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const token = localStorage.getItem("token");
+
+      const data = await fetch(`http://localhost:3000/api/post/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res) => res.json());
+
+      console.log(data);
+
+      setPosts(data.posts);
+    }
+
+    fetchData();
+  }, []);
 
   function handleOpenMenu() {
     if (openMenu == false) {
@@ -62,46 +55,37 @@ export default function Homepage() {
 
   return (
     <>
-      <Header currentPage="home" setMenu={handleOpenMenu} />
-      {openPost == true ? <CreatePost closePost={handleOpenPost} /> : <></>}
+      <Header
+        currentPage="home"
+        setMenu={handleOpenMenu}
+        profilePic={user?.profilePicture}
+      />
+      {openPost == true ? (
+        <CreatePost
+          closePost={handleOpenPost}
+          username={user?.username}
+          profilePic={user?.profilePicture}
+        />
+      ) : (
+        <></>
+      )}
       <div className="relative text-stone-200 flex bg-stone-900">
-        {openMenu == true ? <Menu></Menu> : <></>}
-        <LeftSidebar />
+        {openMenu == true ? <Menu user={user}></Menu> : <></>}
+        <LeftSidebar user={user} />
         <main className="flex-grow z-1 flex flex-col gap-4 py-4 px-2 md:px-6 lg:px-28 w-6/12">
-          <MainPagePost openPost={handleOpenPost} />
-          <Post user="test" postData={data} likes={likes} comments={comments} />
-          <Post
-            user="PowerPlay"
-            postData={{
-              posted_on: new Date(),
-              content:
-                "Did you know that there are monsters within our world? They are called--",
-            }}
-            likes={{ count: 690 }}
-            comments={[]}
-          />
-          <Post
-            user="User"
-            postData={{
-              posted_on: new Date(),
-              content: "i am currently watching Black Clover",
-            }}
-            likes={{ count: 874555 }}
-            comments={[
-              {
-                user: "PowerPlay",
-                content: "Hope you enjoy it!",
-                commented_on: new Date(),
-              },
-              {
-                user: "Naruto Fan 69",
-                content: "Watch Naruto instead smh",
-                commented_on: new Date(),
-              },
-            ]}
-          />
+          <MainPagePost openPost={handleOpenPost} user={user} />
+          {posts.map((item) => (
+            <Post
+              user={user}
+              postUser={item.post.user}
+              postData={item.post}
+              likes={item.likes}
+              comments={item.comments}
+              liked={item.liked}
+            />
+          ))}
         </main>
-        <RightSidebar friends={friends} menuOpen={openMenu} />
+        <RightSidebar />
       </div>
     </>
   );
