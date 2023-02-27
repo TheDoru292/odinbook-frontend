ae5227e809d28c510a602afe7d24c1878315c3fe
thedoru <thedoruv2@gmail.com>
2023-02-27 23:54:07 +0100

    fetch friends from backend and display

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -1,4 +1,33 @@
-export default function RightSidebar({ friends }) {
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function RightSidebar() {
+  const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchStuff() {
+      const userId = localStorage.getItem("id");
+
+      const data = await fetch(
+        `http://localhost:3000/api/user/${userId}/friend`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => res.json());
+
+      if (data.success == true) {
+        setLoading(false);
+      }
+
+      setFriends(data.friends);
+    }
+    fetchStuff();
+  }, []);
+
   return (
     <div
       style={{ height: "calc(100vh - 64px)", top: "62px" }}
@@ -7,14 +36,27 @@ export default function RightSidebar({ friends }) {
       <div className="p-2">
         <p className="font-bold text-lg">Contacts</p>
       </div>
-      {friends.map((item) => {
-        return (
-          <div className="px-2 py-2 rounded-lg flex items-center gap-3 hover:bg-stone-800">
-            <div className="w-9 h-9 bg-red-400 rounded-full"></div>
-            <p className="text-gray-300">{item.user}</p>
-          </div>
-        );
-      })}
+      {loading == true ? (
+        <p>Loading...</p>
+      ) : friends.length === 0 ? (
+        <p>You have no friends :(</p>
+      ) : (
+        friends.map((item) => {
+          console.log(item);
+          return (
+            <Link href={`/profile/${item.second_user.url_handle}`}>
+              <div className="px-2 py-2 rounded-lg flex items-center gap-3 hover:bg-stone-800">
+                <img
+                  src={item.second_user.profile_picture_url}
+                  className="w-9 h-9 rounded-full"
+                  alt=""
+                />
+                <p className="text-gray-300">{item.second_user.username}</p>
+              </div>
+            </Link>
+          );
+        })
+      )}
     </div>
   );
 }
