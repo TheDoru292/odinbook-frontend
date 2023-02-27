f667a4a7da54eafbcd6760b00fdb929a950c9502
thedoru <thedoruv2@gmail.com>
2023-02-27 23:55:17 +0100

    dynamic user profile page

---
+++ pages/profile/[id].js
@@ -0,0 +1,206 @@
+import Header from "@/components/Header";
+import MainPagePost from "@/components/PostDiv";
+import Post from "@/components/Post";
+import Menu from "@/components/Menu";
+import CreatePost from "@/components/CreatePost";
+import ProfileData from "@/components/ProfileData";
+import { useState, useEffect } from "react";
+import { getAllProfileUrl, getUserData, getUserFriends } from "@/lib/profile";
+import { format } from "date-fns";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Profile({ profileUser, friends }) {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openPost, setOpenPost] = useState(false);
+  const [user, setUser] = useState();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [logged, setLogged] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const token = localStorage.getItem("token");
+
+      const bodyData = {
+        token: token || "",
+      };
+
+      const data = await fetch("http://localhost:3000/api/auth/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+      }).then((res) => res.json());
+
+      if (data.code == 400) {
+        setLogged(false);
+      } else {
+        setLogged(true);
+      }
+
+      setUser(data.user);
+    }
+
+    async function fetchPosts() {
+      const token = localStorage.getItem("token");
+
+      const data = await fetch(
+        `http://localhost:3000/api/user/${profileUser._id}/post`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      ).then((res) => res.json());
+
+      console.log(data);
+
+      setPosts(data.posts);
+      setLoading(false);
+    }
+
+    fetchPosts();
+    fetchData();
+  }, []);
+
+  function handleOpenMenu() {
+    if (openMenu == false) {
+      setOpenMenu(true);
+    } else {
+      setOpenMenu(false);
+    }
+  }
+
+  function handleOpenPost() {
+    if (openPost == false) {
+      setOpenPost(true);
+      document.querySelector("body").classList.remove("overflow-visible");
+      document.querySelector("body").classList.add("overflow-hidden");
+    } else {
+      setOpenPost(false);
+      document.querySelector("body").classList.remove("overflow-hidden");
+      document.querySelector("body").classList.add("overflow-visible");
+    }
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>{profileUser.username} - OdinBook</title>
+      </Head>
+      <Header setMenu={handleOpenMenu} profilePic={user?.profilePicture} />
+      {openPost == true ? (
+        <CreatePost
+          username={user?.username}
+          profilePic={user?.profilePicture}
+          closePost={handleOpenPost}
+        />
+      ) : (
+        <></>
+      )}
+      <main className="relative text-stone-200 flex flex-col flex-grow h-100 bg-stone-900">
+        <ProfileData
+          currentPage={"posts"}
+          user={profileUser}
+          friends={friends}
+        />
+        {openMenu == true ? <Menu user={user}></Menu> : <></>}
+        <div className="px-2 xl:px-64 gap-4 flex">
+          <aside className="hidden md:flex m-0 w-5/12 flex-col gap-4 py-4">
+            <div className="bg-stone-800 rounded-xl px-4 py-3">
+              <h2 className="text-lg font-bold">Intro</h2>
+              <p>
+                Registered on:{" "}
+                {format(new Date(profileUser.registered_on), "MMMM, do yyyy")}
+              </p>
+            </div>
+            <div
+              style={{ top: "66px" }}
+              className="sticky bg-stone-800 rounded-xl px-4 py-3"
+            >
+              <div className="flex">
+                <h2 className="flex-grow text-lg font-bold">Friends</h2>
+                <p className="text-lg ">See All Friends</p>
+              </div>
+              <p>{friends.length} friend(s)</p>
+              <div className="mt-2 flex flex-wrap gap-4">
+                {friends.map((friend) => {
+                  return (
+                    <div className="flex-basis flex-grow">
+                      <a
+                        href={`/profile/${encodeURIComponent(
+                          friend.second_user.url_handle
+                        )}`}
+                      >
+                        <img
+                          src={friend.second_user.profile_picture_url}
+                          className="w-16 h-16 xl:w-28 xl:h-28 bg-red-400 rounded-md"
+                          alt=""
+                        />
+                        <p>{friend.second_user.username}</p>
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </aside>
+          <div className="flex flex-col gap-4 py-4 flex-grow ">
+            {profileUser._id !== user?.id ? (
+              <></>
+            ) : (
+              <MainPagePost openPost={handleOpenPost} user={user} />
+            )}
+            {loading == true ? (
+              <p>Loading....</p>
+            ) : posts.length == 0 ? (
+              <p className="flex gap-2 flex-col rounded-xl bg-stone-800 px-4 py-3">
+                User has no posts.
+              </p>
+            ) : (
+              posts.map((item) => {
+                console.log(item);
+                return (
+                  <Post
+                    user={user}
+                    postUser={item.post.user}
+                    postData={item.post}
+                    likes={item.likes}
+                    comments={item.comments}
+                    liked={item.liked}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllProfileUrl();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const user = await getUserData(params.id);
+  const friends = await getUserFriends(user._id);
+
+  return {
+    props: {
+      profileUser: user,
+      friends,
+    },
+  };
+}
