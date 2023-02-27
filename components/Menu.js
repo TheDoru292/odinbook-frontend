d7e7ef80425baf93fd22c787a869e39fd9299b05
thedoru <thedoruv2@gmail.com>
2023-02-27 23:52:27 +0100

    placeholder profile picture to actual

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
-export default function Menu() {
+export default function Menu({ user }) {
   return (
     <div className="fixed shadow-[0_0_6px] shadow-stone-900 gap-4 rounded-b-md flex flex-col right-0 w-[320px] px-4 py-2 z-10 bg-stone-800 mr-4">
-      <div className="flex shadow-[0_0_12px] shadow-stone-900 rounded-md drop-shadow-2xl gap-2 p-3">
-        <div className="rounded-full w-9 h-9 bg-red-400"></div>
-        <p className="self-center font-bold">User</p>
-      </div>
+      <Link href={`/profile/${user?.userhandle}`}>
+        <div className="hover:bg-stone-700 flex shadow-[0_0_12px] shadow-stone-900 rounded-md drop-shadow-2xl gap-2 p-3">
+          <img
+            src={`${user?.profilePicture}`}
+            className="w-9 h-9 rounded-full"
+            alt=""
+          />
+          <p className="self-center font-bold">{user?.username}</p>
+        </div>
+      </Link>
       <div className="p-2 rounded-lg hover:bg-stone-700">
         <Link href="/logout" className="flex gap-3">
           <div className="w-9 h-9 bg-red-400 rounded-full"></div>
