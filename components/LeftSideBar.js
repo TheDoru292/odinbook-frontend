d7b49962be2ac52712a0b4470be655288fa071b2
thedoru <thedoruv2@gmail.com>
2023-02-27 23:51:52 +0100

    placeholder to actual data

--- components/LeftSideBar.js
+++ components/LeftSideBar.js
@@ -1,39 +1,45 @@
 import Link from "next/link";
 
-export default function LeftSidebar() {
+export default function LeftSidebar({ user }) {
   return (
     <div
       style={{ height: "calc(100vh - 64px)", top: "62px" }}
       className="hidden xl:block py-4 pl-2 h-full w-[320px] m-0 sticky"
     >
-      <Link href="/profile/test">
+      <Link href={`/profile/${user?.userhandle}`}>
         <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-          <div className="w-9 h-9 bg-red-400 rounded-full"></div>
-          <p className="self-center">Username</p>
+          <img
+            src={`${user?.profilePicture}`}
+            className="w-9 h-9 rounded-full"
+            alt=""
+          />
+          <p className="self-center">{user?.username}</p>
+        </div>
+      </Link>
+      <Link href={"/friend"}>
+        <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
+          <img src="account-multiple.svg" className="w-9 h-9 " alt="" />
+          <p className="self-center">Friends</p>
         </div>
       </Link>
       <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
-        <p className="self-center">Friends</p>
-      </div>
-      <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+        <img src="history.svg" className="w-9 h-9 " alt="" />
         <p className="self-center">Most recent</p>
       </div>
       <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+        <img src="account-group.svg" className="w-9 h-9 " alt="" />
         <p className="self-center">Groups</p>
       </div>
       <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+        <img src="shopping.svg" className="w-9 h-9 " alt="" />
         <p className="self-center">Marketplace</p>
       </div>
       <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+        <img src="video.svg" className="w-9 h-9 " alt="" />
         <p className="self-center">Watch</p>
       </div>
       <div className="hover:bg-stone-700 px-2 py-2 rounded-lg flex gap-4">
-        <div className="w-9 h-9 bg-red-400 rounded-full"></div>
+        <img src="thumb-up.svg" className="w-9 h-9 " alt="" />
         <p className="self-center">Liked Posts</p>
       </div>
     </div>
